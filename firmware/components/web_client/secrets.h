c8f06393e49365fdc09c7034f19c93cdbb7bd37f
Diego Rodríguez Riera <riera90@protonmail.com>
2022-07-07 01:23:50 +0200

    fixed monostable and improved pcb layout

--- firmware/components/web_client/secrets.h
+++ firmware/components/web_client/secrets.h
@@ -4,7 +4,7 @@
 
 #define SERVER_ADDR "http://192.168.1.105:8000"
 #define SERVER_SECRET "server shared secret code"
-#define SSID "PTVTELECOM_2SUT"
-#define PSK "2cgruf27anqh"
+#define SSID "honeypot"
+#define PSK "RofznxG38E6"
 
 #endif
